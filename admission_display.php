4d10ef74045239cc1eaaa3130fab0802448c5125
Harsha Sharma <91453790+Harsha-1304@users.noreply.github.com>
2022-05-03 00:01:38 +0530

    Update admission_display.php

--- admission_display.php
+++ admission_display.php
@@ -15,31 +15,66 @@ $class = $_GET['class'];
 	<title></title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
+<style>
+  .board{
+    /*border: 2px solid black;*/
+    background-image: url('images/board.jpg');
+    /*height: 700px;*/
+    /*width: 100%;*/
+    /*max-width: 100%;*/
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    color: white;
+  }
+
+  .board div{
+    transform: rotate(-5deg);
+  }
+
+  .rotation p{
+    margin-bottom: 7px !important;
+  }
+</style>
 <body>
 
+<h1 class="text-center">My Details</h1>
 
 <div class="container">
   <div class="row">
-    <div class="col-md-6 text-center aos-init aos-animate" data-aos="fade-right" data-aos-delay="200"><img src="images/copy.png" style="max-width: 100%;"></div>
-    <div class="col-md-6">
-      <h1>My Details</h1>
+    <div class="col-md-6 text-center aos-init aos-animate" data-aos="fade-right" data-aos-delay="200">
+      <!-- <img src="images/copy.png" style="max-width: 100%;"> -->
+      <img src="images/peach.png" style="width: 100px;">
+    </div>
+    <div class="col-md-6 board">
       <?php
-          $i = 1;
-          $user_id = $_SESSION['id'];
-          include("dbcon.php");
-          $data = "select * from admission where user_id = $user_id and class = '$class' ";
-          $result = mysqli_query($con,$data); // returns a object
-          //echo "<pre>";
-          //print_r($result);
+        $i = 1;
+        $user_id = $_SESSION['id'];
+        include("dbcon.php");
+        $data = "select * from admission where user_id = $user_id and class = '$class' ";
+        $result = mysqli_query($con,$data); // returns a object
+        //echo "<pre>";
+        //print_r($result);
 
-          while($d = mysqli_fetch_array($result)){
-            //echo "<pre>";
-            //print_r($d);
+        while($d = mysqli_fetch_array($result)){
+          //echo "<pre>";
+          //print_r($d);
       ?>
-      <div>
+      <div style="margin: 84px;" class="rotation">
         <p>S No. - <?php echo $i++ ?></p>
         <p>Parent Name - <?php echo $d['parentname'] ?></p>
         <p>Email - <?php echo $d['email'] ?></p>
+        <p>Relationship - <?php echo $d['relationship'] ?></p>
+        <p>Phone - <?php echo $d['phone'] ?></p>
+        <p>Student Name - <?php echo $d['studentname'] ?></p>
+        <p>Student DOB - <?php echo $d['studentdob'] ?></p>
+        <p>Address - <?php echo $d['address'] ?></p>
+        <p>Class - <?php echo $d['class'] ?></p>
+        <p>Status - <?php echo $d['status'] ?></p>
+        <p>Comment - <?php echo $d['comment'] ?></p>
+        <p>
+          Operations - <a href="admission_view.php?id=<?php echo $d['id'] ?>" class="btn btn-primary">View</a> <a href="admission_edit.php?id=<?php echo $d['id'] ?>" class="btn btn-info">Edit</a>
+        </p>
       </div>
       <?php
         }
